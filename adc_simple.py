3f55f40c60f6c81e3c34448f9f253589151e2d3f
Fyodor Egorov <egorov.fd@phystech.edu>
2022-03-30 10:44:15 +0300

    binssearch & healthbar

--- adc_simple.py
+++ adc_simple.py
@@ -1,9 +1,14 @@
 from mimetypes import init
+import re
+from weakref import ref
 import RPi.GPIO as io
+import time
+import math
 
 dacPins = [26, 19, 13, 6, 5, 11, 9, 10]
 comparatorPin = 4
 analogPowerPin = 17
+indicator = [21, 20, 16, 12, 7, 8, 25, 24]
 
 io.setmode(io.BCM)
 
@@ -28,17 +33,47 @@ def dacWrite(dacPins, value):
 
 def voltageBinarySearch(comp, pins):
     digits = len(pins)
+    scale = 2**(digits - 1)
+    reference = scale - 1 #!
+
+    vector = []
+
+    for i in range(digits):
+        dacWrite(reference)
+        time.sleep(0.00001)
+        scale /= 2
+        if io.input(comparatorPin) == 0:
+            vector.append(1)
+            reference += scale
+        else:
+            vector.append(0)
+            reference -= scale
+
 
 def bruteSearch(comp, pins):
     scale = 2**len(pins)
     for value in range(scale):
         dacWrite(dacPins, value)
+        time.sleep(0.00001)
         if io.input(comp) == 0:
             return value
 
+def healthbar(leds, value):
+    io.output(leds, 0)
+    size = len(leds)
+    thres = size*(value + 1)
+    for i in range(size):
+        if thres < i + 1:
+            break
+        io.output(leds[i], 1)
 
 try:
     while True:
-        bruteSearch(comparatorPin, dacPins)
+        #bruteSearch(comparatorPin, dacPins)
+        vec = voltageBinarySearch(comparatorPin, dacPins)
+        value = bin2decNormalized(vec)
+        healthbar(indicator, value)
+        print("{:.3f}".format(value * 3.3) + " VOLTS")
+        time.sleep(0.5)
 finally:
     io.cleanup()   
